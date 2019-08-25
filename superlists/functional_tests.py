a51398300d24c3b9a14c22d5595ce86a86b1368f
lengxia <13071142513@163.com>
2019-08-25 09:59:55 +0800

    待办事项修改

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -5,11 +5,6 @@ from selenium import webdriver
 import unittest
 import time
 
-
-# browser = webdriver.Firefox()
-# browser.get('http://localhost:8000')
-
-# assert  'Django ' in browser.title
 from selenium.webdriver.common.keys import Keys
 
 
@@ -22,32 +17,34 @@ class NewVisitorTest(unittest.TestCase):
 
         self.browser.quit()
 
+    def check_for_row_in_list_table(self,row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text,[row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
 
         self.browser.get("http://localhost:8000")
-        self.assertIn('To-Do',self.browser.title)
-        header_text = self.browser.find_element_by_tag_name('h1').text
-        self.assertIn('To-Do',header_text)
+        # # self.assertIn('To-Do',self.browser.title)
+        # header_text = self.browser.find_element_by_tag_name('h1').text
+        # # self.assertIn('To-Do',header_text)
+
+
 
         # 应用邀请输入一个待办事项
+
+
+
         inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertEqual(
-            inputbox.get_attribute('placeholder'),
-            'Enter a to-do item'
-        )
         # 输入框输入"购买孔雀毛"
-        inputbox.send_keys('购买孔雀毛')
+        inputbox.send_keys('购买孔雀羽毛')
 
         # 按回车键后，待办表格中显示 "购买孔雀毛"
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text =='1: 购买孔雀毛' for row in rows),
-            "New to-do item did not appear in table"
-        )
+        self.check_for_row_in_list_table('1: 购买孔雀羽毛')
+
 
         self.fail('Finish the test')
 
