cd37d811db47a3cf9dbde774edba7593869d6e66
lengxia <13071142513@163.com>
2019-09-01 16:01:00 +0800

    添加使用STAGING_SERVER

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -5,12 +5,16 @@ import unittest
 from django.test import LiveServerTestCase
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium.common.exceptions import WebDriverException
+import os
 
 MAX_WAIT = 10
 class NewVisitorTest(StaticLiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
+        staging_server = os.environ.get('STAGING_SERVER')
+        if staging_server:
+            self.live_server_url = 'http://' + staging_server
 
     def tearDown(self):
         self.browser.refresh()
